b3524747649c6ba04eec65a893787eaae2a477f4
GPLme <524019351@qq.com>
2019-08-01 22:16:10 +0800

    更新 help.h

--- help.h
+++ help.h
@@ -6,27 +6,29 @@
 using namespace std;
 
 ofstream *ofile=nullptr;
+string lastPath;
 
 void writeFile(string path, string content)
 {
-        if (ofile == nullptr)
-        {
-                ofile = new ofstream;
-                ofile->open(path, std::ios::app);
-        }
-        (*ofile) << content << endl;   //标题写入文件
+	if (ofile == nullptr || path!=lastPath)
+	{
+		lastPath = path;
+		ofile = new ofstream;
+		ofile->open(path, std::ios::app);
+	}
+	(*ofile) << content << endl;   //标题写入文件
 }
 
 string readFile(string file)
 {
-        ifstream infile;
-        infile.open(file.data());   //将文件流对象与文件连接起来
-        string content;
-        string lineContent;
-        while (getline(infile, lineContent))
-            content+=lineContent+"\n";
-        infile.close();             //关闭文件输入流
-        return content.substr(0,content.size()-1);
+	ifstream infile;
+	infile.open(file.data());   //将文件流对象与文件连接起来
+	string content;
+	string lineContent;
+	while (getline(infile, lineContent))
+		content += lineContent + "\n";
+	infile.close();             //关闭文件输入流
+	return content.substr(0, content.size() - 1);
 }
 
 void string_replace(std::string &strBig, const std::string &strsrc, const std::string &strdst)
